7fbb43040a0454f1c78bff29db8056069604723d
gongxuanzhang <gongxuanzhang@foxmail.com>
2023-02-08 16:43:58 +0800

    init a empty project

---
+++ src/main/java/org/gongxuanzhang/supportdemo/SupportDemoApplication.java
@@ -0,0 +1,16 @@
+package org.gongxuanzhang.supportdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gongxuanzhangmelt@gmail.com
+ */
+@SpringBootApplication
+public class SupportDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gongxuanzhang/supportdemo/SupportDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gongxuanzhang.supportdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
