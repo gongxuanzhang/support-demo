b9f66811cf90abc6eab9117f06dfc1280898545b
gongxuanzhang <gongxuanzhang@foxmail.com>
2023-02-21 10:24:30 +0800

    to sue

--- src/main/java/org/gongxuanzhang/supportdemo/ComplexFill.java
+++ src/main/java/org/gongxuanzhang/supportdemo/ComplexFill.java
@@ -1,8 +1,14 @@
 package org.gongxuanzhang.supportdemo;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.util.ListUtils;
+import com.alibaba.excel.write.handler.WorkbookWriteHandler;
+import com.alibaba.excel.write.handler.context.WorkbookWriteHandlerContext;
+import org.apache.poi.ss.usermodel.Sheet;
 
-import java.net.URL;
+import java.io.File;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author gxz gongxuanzhang@foxmail.com
@@ -10,9 +16,29 @@ import java.net.URL;
 public class ComplexFill {
 
     public static void main(String[] args) {
-        URL resource = ComplexFill.class.getClassLoader().getResource("demo.xlsx");
+        File file = new File("demo.xlsx");
+        // 分页查询数据
+        EasyExcel.write(file, DemoData.class)
+                .sheet("模板").registerWriteHandler(new WorkbookWriteHandler() {
+                    @Override
+                    public void afterWorkbookDispose(WorkbookWriteHandlerContext context) {
+                        Sheet sheet = context.getWriteContext().writeSheetHolder().getSheet();
+                        //   这里手动合并
+                    }
+                })
+                .doWrite(ComplexFill::data);
+    }
 
-        EasyExcel.read(resource.getFile(), Jld.class,new MyLister()).sheet(0).headRowNumber(9).doReadSync();
+    private static List<DemoData> data() {
+        List<DemoData> list = ListUtils.newArrayList();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
     }
 
 
